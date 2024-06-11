f42d7ba2da699a751cfdcefaac2502cc7a6066b0
saadqureshi-1 <q.saad.9315@gmail.com>
2024-06-11 11:13:24 +0000

    add seed to create superuser

--- init_db.py
+++ init_db.py
@@ -1,7 +1,10 @@
 from app import create_app, db
-
+from seed import seed_admin_user
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully.")
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("Database tables created successfully.")
+        seed_admin_user()
+init_db()
---
+++ seed.py
@@ -0,0 +1,21 @@
+from app import  db
+from app.models import User
+
+def seed_admin_user():
+    # Create an admin user
+    email="admin@gmail.com"
+    password="admin"
+    admin_user = User(
+        email=email,
+        is_admin=True
+    )
+    admin_user.set_password(password)
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        print('Superuser already exists!')
+        return
+    
+    db.session.add(admin_user)
+    db.session.commit()
+
+    print('Admin user seeded successfully!')
