62712dec5d760e8de3cf4f3cdd64a94b9ef861b2
saadqureshi-1 <q.saad.9315@gmail.com>
2024-06-12 08:41:03 +0000

    email varification fix

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 import logging
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 mail = Mail()
-load_dotenv()
+# load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    app.config['MAIL_SERVER'] = 'smtp.thehexaa.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.getenv('EMAIL_HOST')
-    app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+    # app.config['MAIL_SERVER'] = 'smtp.thehexaa.com'
+    # app.config['MAIL_PORT'] = 587
+    # app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_USERNAME'] = os.getenv('EMAIL_HOST')
+    # app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+
+
+    # Flask-Mail configuration
+    app.config['MAIL_SERVER'] = 'mail.thehexaa.com'
+    app.config['MAIL_PORT'] = 25
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USERNAME'] = None
+    app.config['MAIL_PASSWORD'] = None
+    app.config['MAIL_DEFAULT_SENDER'] = 'daily-reports@thehexaa.com'
 
     db.init_app(app)
     mail.init_app(app)
